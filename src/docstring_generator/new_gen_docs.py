83700d382cd2725eadb5c9c83e4406d74f187304
felix <felixeisenmenger@gmx.net>
2026-07-07 21:33:26 +0200

    support multiple files for a good pre-commit hook experience

--- src/docstring_generator/new_gen_docs.py
+++ src/docstring_generator/new_gen_docs.py
@@ -5,28 +5,29 @@ import docstring_generator_ext
 
 
 @click.command()
-@click.argument("path")
+@click.argument("paths", nargs=-1, required=True)
 @click.option(
     "--style", default="numpy", help="Docstring style [numpy, rest, google].", show_default=True
 )
-def main(path: str, style: str) -> None:
-    path_ = pathlib.Path(path)
-
+def main(paths: tuple[str, ...], style: str) -> None:
     docstring_style = docstring_generator_ext.DocstringFormatStyle.NUMPY
     if style == "rest":
         docstring_style = docstring_generator_ext.DocstringFormatStyle.reST
     if style == "google":
         docstring_style = docstring_generator_ext.DocstringFormatStyle.GOOGLE
 
-    if not path_.exists():
-        return
+    for path in paths:
+        path_ = pathlib.Path(path)
 
-    if path_.is_dir():
-        for file in path_.glob("**/*.py"):
-            docstring_generator_ext.parse_file(file.absolute().as_posix(), docstring_style)
+        if not path_.exists():
+            continue
 
-    if path_.is_file() and path_.name.endswith(".py"):
-        docstring_generator_ext.parse_file(path_.absolute().as_posix(), docstring_style)
+        if path_.is_dir():
+            for file in path_.glob("**/*.py"):
+                docstring_generator_ext.parse_file(file.absolute().as_posix(), docstring_style)
+
+        if path_.is_file() and path_.name.endswith(".py"):
+            docstring_generator_ext.parse_file(path_.absolute().as_posix(), docstring_style)
 
 
 if __name__ == "__main__":
